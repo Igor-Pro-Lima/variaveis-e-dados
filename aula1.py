fffbf32867048d261382a7cbba7ae79b7541387e
Igor Lima <igor.pro.lima@gmail.com>
2024-07-10 18:05:16 -0300

    Initial commit

---
+++ aula1.py
@@ -0,0 +1,18 @@
+idade: int
+salario: float
+altura: float
+genero: str
+nome: str
+
+idade = 20
+salario = 4000.5
+altura = 1.75
+genero = "M"
+nome = "Igor Lima"
+
+print(f"IDADE = {idade}")
+print(f"SALARIO = {salario:.2f}")
+print(f"ALTURA = {altura:.2f}")
+print(f"GENERO = {genero}")
+print(f"NOME = {nome}")
+
